2db6abc1f591b9d461c9bf57d3563106d645875a
quadri-usman <105228467+quadri-usman@users.noreply.github.com>
2022-07-02 03:17:01 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+      <div>
+        <div>
+          <h1>THE TRUST FOUNDATION</h1>
+          <p class="p1">Science, Technology and Innovation</p>        
+          <button class="btn btn-default btn-lg"><i class="fa-solid fa-paw"></i> APPLY</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
